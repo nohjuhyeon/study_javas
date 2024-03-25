5ef4ee13417e30d6f848e93b2accc9bd5b4b477c
nohjuhyeon <njh2720@gmail.com>
2024-03-25 06:14:33 +0000

    quest

---
+++ src/quests/WhilesIfs.java
@@ -0,0 +1,32 @@
+package quests;
+
+import java.util.Scanner;
+public class WhilesIfs {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        boolean whileFlag = true;
+        while (whileFlag) {
+            int input_score = scanner.nextInt();
+            if (input_score == -1) {
+                break;
+            }    
+            else if (input_score >= 90) {
+                System.out.println("A");
+            } 
+            else if (input_score >= 80) {
+                System.out.println("B");
+            } 
+            else if (input_score >= 70) {
+                System.out.println("C");
+            } 
+            else {
+                System.out.println("F");
+                
+            }
+        }
+        
+        scanner.close();
+    }
+}
+
+
