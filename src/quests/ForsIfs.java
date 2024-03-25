5eada3a8e41b7564b2d920ac80432bd0cb383a53
nohjuhyeon <njh2720@gmail.com>
2024-03-25 03:21:48 +0000

    quest

--- src/quests/ForsIfs.java
+++ src/quests/ForsIfs.java
@@ -8,7 +8,7 @@ public class ForsIfs {
         for ( int for_num = 1 ; for_num <= input_num ; for_num = for_num + 1){
             int mod_num = for_num %4;
             if (mod_num == 0){
-                System.out.println(for_num/4 + "* 4 = " + for_num  );
+                System.out.println(for_num + " / 4 = " + for_num/4  );
             }
         }
         
