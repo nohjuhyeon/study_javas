34d9add757dc67dd494f1ebeace3cc34b5a515c6
nohjuhyeon <njh2720@gmail.com>
2024-03-25 03:50:36 +0000

    quest

--- src/quests/ForsIfs.java
+++ src/quests/ForsIfs.java
@@ -7,7 +7,7 @@ public class ForsIfs {
         int input_num = scanner.nextInt();
         for ( int for_num = 1 ; for_num <= input_num ; for_num = for_num + 1){
             int result_num = (int)Math.pow(2, for_num);
-            int mod_num = result_num%4;
+            int mod_num = result_num%8;
             if (mod_num == 0){
                 System.out.println("2 ** " + for_num + " = " +result_num);
             }
@@ -26,7 +26,7 @@ public class ForsIfs {
 //             for (int i = 1 ; i <= for_num;i = i+1){
 //                 check_num = check_num * 2;
 //             }
-//             int mod_num = check_num%4;
+//             int mod_num = check_num%8;
 //             if (mod_num == 0){
 //                 System.out.println("2 ** " + for_num + " = " +check_num);
 //             }
